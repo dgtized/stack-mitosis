61e0ee3b2ae688120cf9253a9f90b9873d72b025
Charles Comstock <dgtized@gmail.com>
2018-12-21 21:01:11 -0600

    More docs and start to sketch out some operations

--- src/stack_mitosis/core.clj
+++ src/stack_mitosis/core.clj
@@ -3,13 +3,46 @@
 
 (def rds (aws/client {:api :rds}))
 
+(defn tags
+  [arn]
+  {:op :ListTagsForResource
+   :request {:ResourceName arn}})
+
+(defn rename
+  [old new]
+  {:op :ModifyDBInstance
+   :request {:DbInstanceIdentifier old
+             :NewDBInstanceIdentifier new
+             :ApplyImmediately true}})
+
+(defn delete
+  [id]
+  {:op :DeleteDBInstance
+   :request {:DbInstanceIdentifier id
+             :SkipFinalSnapshot true}})
+
+(defn promote
+  [id]
+  {:op :PromoteReadReplica
+   :request {:DbInstanceIdentifier id}})
+
 (comment
   (keys (aws/ops rds))
+  (aws/doc rds :CreateDBInstance) ;; for testing
+  (aws/doc rds :DescribeDBInstances)
+  (aws/doc rds :CreateDBInstanceReadReplica)
+  (aws/doc rds :PromoteReadReplica)
+  (aws/doc rds :ModifyDBInstance)
+  (aws/doc rds :DeleteDBInstance)
+  (aws/doc rds :ListTagsForResource)
 
-  (map (fn [{:keys [DBInstanceIdentifier ReadReplicaDBInstanceIdentifiers]}]
+  (map (fn [{:keys [DBInstanceIdentifier ReadReplicaDBInstanceIdentifiers DBInstanceArn]}]
          {:id DBInstanceIdentifier
+          :arn DBInstanceArn
           :replicas ReadReplicaDBInstanceIdentifiers})
-       (:DBInstances (aws/invoke rds {:op :DescribeDBInstances}))))
+       (:DBInstances (aws/invoke rds {:op :DescribeDBInstances})))
+
+  (aws/invoke rds (tags "")))
 
 (defn -main []
   (println "hi"))
