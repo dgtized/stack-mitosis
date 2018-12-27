1eaf8b4fa0b2ee776755e925c25eab85f86a45c6
Charles Comstock <dgtized@gmail.com>
2018-12-27 15:35:43 -0600

    Clarified topological-sort

--- src/stack_mitosis/helpers.clj
+++ src/stack_mitosis/helpers.clj
@@ -9,14 +9,23 @@
     m))
 
 (defn topological-sort
+  "Returns a topologically sorted list of nodes from a graph represented as a map of
+  key to set of dependent nodes. Returns nil if graph contains a cyclic dependency."
   [graph]
-  (letfn [(nodes [g] (apply set/union (keys g) (vals g)))]
+  (letfn [(nodes [g] (apply set/union (keys g) (vals g)))
+          (no-dependencies? [g n] (empty? (get g n (set nil))))]
     (loop [ret [] graph graph]
       (if (empty? (nodes graph))
         ret
-        (let [no-deps (sort (set (filter #(empty? (get graph % (set nil))) (nodes graph))))
-              graph' (reduce-kv #(assoc %1 %2 (apply disj %3 no-deps)) {}
-                                (apply dissoc graph no-deps))]
+        (let [no-deps
+              (->> (nodes graph)
+                   (filter (partial no-dependencies? graph))
+                   sort set)
+              graph'
+              (reduce-kv (fn [g node edges]
+                           (assoc g node (set/difference edges no-deps)))
+                         {}
+                         (apply dissoc graph no-deps))]
           (if (empty? no-deps)
             nil ;; cyclic dependencies
             (recur (concat ret no-deps) graph')))))))
