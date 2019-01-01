b0c1ee02c68f12a8fff96496e265be42f4d1dc15
Charles Comstock <dgtized@gmail.com>
2019-01-01 15:24:29 -0600

    actually limit to N, and run timeout before check

--- src/stack_mitosis/wait.clj
+++ src/stack_mitosis/wait.clj
@@ -2,24 +2,22 @@
   (:require [clojure.core.async :as a]))
 
 (defn block [time prob]
-  (a/thread
-    (a/timeout time)
-    (println "waited" time)
-    (< (rand) prob)))
+  (Thread/sleep time)
+  (println "waited" time)
+  (< (rand) prob))
 
 (defn waiter [pred-fn delay max-attempts]
   (let [completed (a/chan)]
     (a/go-loop [attempt 0]
-      (if-let [resp (a/<! (pred-fn))]
+      (a/<! (a/timeout delay))
+      (if-let [resp (pred-fn)]
         (a/>! completed resp)
-        (if (> attempt max-attempts)
-          (a/>! completed :max-attempts)
-          (do
-            (a/<! (a/timeout delay))
-            (recur (inc attempt))))))
+        (if (< attempt max-attempts)
+          (recur (inc attempt))
+          (a/>! completed :max-attempts))))
     completed))
 
 (comment
-  (a/<!! (waiter #(block 100 0.2) 50 5)))
+  (a/<!! (waiter #(block 10 0.2) 100 5)))
 
 ;; (defn poll-until [predicate {:timeout 1000 :delay 100}])
