73d11377f042a07ac6d5b1464877bcfa8bd895f6
Charles Comstock <dgtized@gmail.com>
2019-06-18 13:29:41 -0500

    mockup for command line parsing

---
+++ src/stack_mitosis/cli.clj
@@ -0,0 +1,25 @@
+(ns stack-mitosis.cli
+  (:require [clojure.tools.cli :as cli]))
+
+(def cli-options
+  [["-s" "--source SRC" "Root identifier of database tree to copy from"]
+   ["-t" "--target DST" "Root identifier of database tree to copy over"]
+   [nil "--restart CMD", "Blocking script to restart application."]
+   ["-h" "--help"]])
+
+(defn parse-args [args]
+  (let [{:keys [options errors summary]}
+        (cli/parse-opts args cli-options)]
+    (cond
+      (:help options)
+      {:exit-msg summary :ok true}
+      errors
+      {:exit-msg [summary errors] :ok false}
+      :else
+      options)))
+
+(defn -main [& args]
+  (parse-args args))
+
+(comment
+  (parse-args ["a" "--source" "production" "--target" "staging"]))
