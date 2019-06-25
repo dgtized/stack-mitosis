8a7af359369f51616b868262fc5eaf8ad6a8b2f1
Charles Comstock <dgtized@gmail.com>
2019-06-25 13:01:32 -0500

    Sketch out converting command options to plan or evaluate operations

--- src/stack_mitosis/cli.clj
+++ src/stack_mitosis/cli.clj
@@ -1,11 +1,14 @@
 (ns stack-mitosis.cli
-  (:require [clojure.tools.cli :as cli]))
+  (:require [clojure.tools.cli :as cli]
+            [stack-mitosis.interpreter :as interpreter]
+            [stack-mitosis.planner :as plan]))
 
 ;; maybe allow specifying mfa token / role arn for sudo?
 (def cli-options
   [["-s" "--source SRC" "Root identifier of database tree to copy from"]
    ["-t" "--target DST" "Root identifier of database tree to copy over"]
    [nil "--restart CMD", "Blocking script to restart application."]
+   ["-p" "--plan", "Display expected flightplan for operation."]
    ["-h" "--help"]])
 
 (defn parse-args [args]
@@ -19,8 +22,27 @@
       :else
       options)))
 
+(defn process [options]
+  (let [rds interpreter/rds
+        plan (plan/replace-tree (interpreter/databases rds)
+                                (:source options) (:target options))]
+    (cond (:plan options)
+          (doseq [op plan]
+            (pr op)
+            (newline))
+          :else
+          (interpreter/evaluate-plan rds plan))))
+
 (defn -main [& args]
-  (parse-args args))
+  (let [{:keys [ok exit-msg] :as options} (parse-args args)]
+    (when exit-msg
+      (println exit-msg)
+      (System/exit (if ok 0 1)))
+    (process options)
+    (System/exit 0)
+    ))
 
 (comment
-  (parse-args ["a" "--source" "production" "--target" "staging"]))
+  (println (parse-args ["--source" "production" "--target" "staging"
+                        "--restart" "./restart.sh" "production"]))
+  (process (parse-args ["--source" "mitosis-root" "--target" "mitosis-alpha" "--plan"])))
