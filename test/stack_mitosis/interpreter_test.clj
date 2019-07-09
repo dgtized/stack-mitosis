04965457787965fafb614ba9d945f6365a0ad5c6
Charles Comstock <dgtized@gmail.com>
2019-07-09 16:03:38 -0500

    Verify output and logging output

--- test/stack_mitosis/interpreter_test.clj
+++ test/stack_mitosis/interpreter_test.clj
@@ -1,24 +1,34 @@
 (ns stack-mitosis.interpreter-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
             [clojure.tools.logging.test :as tlog]
             [stack-mitosis.interpreter :as sut]
             [stack-mitosis.operations :as op]))
 
 (defn- eval-plan [ops]
-  (tlog/with-log (with-out-str (sut/evaluate-plan nil ops))
-    (map :message (tlog/the-log))))
+  (tlog/with-log
+    (let [r (with-out-str (sut/evaluate-plan nil ops))]
+      {:output (str/split-lines r)
+       :logging (map :message (tlog/the-log))})))
 
 (deftest evaluate-plan
   (with-redefs [sut/databases (fn [x] [])]
-    (is (= ["Invoking {:op :shell-command, :request {:cmd \"false\"}}"
-            "Executing [false] failed with status 1"]
+    (is (= {:output
+            ["  exit: 1"]
+            :logging
+            ["Invoking {:op :shell-command, :request {:cmd \"false\"}}"
+             "Executing [false] failed with status 1"]}
            (eval-plan [(op/shell-command "false")
                        (op/shell-command "true")]))
         "early exit")
-    (is (= ["Invoking {:op :shell-command, :request {:cmd \"true\"}}"
-            "{:ok Executing [true] succeeded}"
-            "Invoking {:op :shell-command, :request {:cmd \"true\"}}"
-            "{:ok Executing [true] succeeded}"]
+    (is (= {:output
+            ["  exit: 0"
+             "  exit: 0"]
+            :logging
+            ["Invoking {:op :shell-command, :request {:cmd \"true\"}}"
+             "{:ok Executing [true] succeeded}"
+             "Invoking {:op :shell-command, :request {:cmd \"true\"}}"
+             "{:ok Executing [true] succeeded}"]}
            (eval-plan [(op/shell-command "true")
                        (op/shell-command "true")]))
         "execute all")))
