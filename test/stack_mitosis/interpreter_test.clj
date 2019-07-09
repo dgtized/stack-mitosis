f19e566d26546016d0983dff223f0ded47e9d4c3
Charles Comstock <dgtized@gmail.com>
2019-07-09 16:02:30 -0500

    Use logging.test to capture log output correctly in tests
    clj -Atest was failing, but cider test was not

--- test/stack_mitosis/interpreter_test.clj
+++ test/stack_mitosis/interpreter_test.clj
@@ -1,29 +1,23 @@
 (ns stack-mitosis.interpreter-test
-  (:require [stack-mitosis.interpreter :as sut]
-            [clojure.test :refer :all]
-            [stack-mitosis.operations :as op]
-            [clojure.string :as str]))
-
-(defn- clean-logging [output]
-  (map #(str/replace-first % #".*\|.*\|.*\| " "")
-       (str/split-lines output)))
+  (:require [clojure.test :refer :all]
+            [clojure.tools.logging.test :as tlog]
+            [stack-mitosis.interpreter :as sut]
+            [stack-mitosis.operations :as op]))
 
 (defn- eval-plan [ops]
-  (clean-logging (with-out-str (sut/evaluate-plan nil ops))))
+  (tlog/with-log (with-out-str (sut/evaluate-plan nil ops))
+    (map :message (tlog/the-log))))
 
 (deftest evaluate-plan
   (with-redefs [sut/databases (fn [x] [])]
     (is (= ["Invoking {:op :shell-command, :request {:cmd \"false\"}}"
-            "  exit: 1"
             "Executing [false] failed with status 1"]
            (eval-plan [(op/shell-command "false")
                        (op/shell-command "true")]))
         "early exit")
     (is (= ["Invoking {:op :shell-command, :request {:cmd \"true\"}}"
-            "  exit: 0"
             "{:ok Executing [true] succeeded}"
             "Invoking {:op :shell-command, :request {:cmd \"true\"}}"
-            "  exit: 0"
             "{:ok Executing [true] succeeded}"]
            (eval-plan [(op/shell-command "true")
                        (op/shell-command "true")]))
