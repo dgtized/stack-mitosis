138e26b001e1b7528c740e97e3ffc605a1442b2b
Charles Comstock <dgtized@gmail.com>
2020-04-24 16:24:21 -0500

    Add lookup/clone-replica-attributes for looking up original values

--- src/stack_mitosis/lookup.clj
+++ src/stack_mitosis/lookup.clj
@@ -27,3 +27,38 @@
   [instances db-id]
   (get (by-id instances db-id)
        :ReadReplicaDBInstanceIdentifiers))
+
+(defn clone-replica-attributes
+  "Creates a list of additional attributes to clone from original instance into
+  the newly created replica instance."
+  [original]
+  (let [attributes-to-clone
+        [:CopyTagsToSnapshot
+         :MonitoringRoleArn
+         :MonitoringInterval
+         :PubliclyAccessible
+         :AutoMinorVersionUpgrade
+         :DBInstanceClass
+         :PerformanceInsightsKMSKeyId
+         :KmsKeyId
+         :PerformanceInsightsRetentionPeriod
+         :EnableCloudwatchLogsExports
+         :ProcessorFeatures
+         :Iops
+         :StorageType
+         :MultiAZ]]
+    (-> original
+        ;; copy as-is with no translation
+        (select-keys attributes-to-clone)
+        ;; Attributes requiring custom rules to extract from original and
+        ;; translate to key for clone-replica request
+        (merge (into {}
+                     (remove (fn [[k v]] (or (nil? v) (and (seq? v) (empty? v))))
+                             {:VpcSecurityGroupIds (map :VpcSecurityGroupId (:VpcSecurityGroups original))
+                              :EnablePerformanceInsights (:PerformanceInsightsEnabled original)
+                              :EnableIAMDatabaseAuthentication (:IAMDatabaseAuthenticationEnabled original)
+                              ;; TODO map for names on original
+                              ;; :DBParameterGroupName
+                              ;; :OptionGroupName
+                              ;; :DBSubnetGroupName
+                              }))))))
--- test/stack_mitosis/lookup_test.clj
+++ test/stack_mitosis/lookup_test.clj
@@ -14,3 +14,20 @@
     (is (= 0 (lookup/position instances "a")))
     (is (= 1 (lookup/position instances "b")))
     (is (= nil (lookup/position instances "missing")))))
+
+(deftest clone-replica-attributes
+  (let [instance {:DBInstanceIdentifier "a"
+                  :Iops 100
+                  :VpcSecurityGroups [{:VpcSecurityGroupId "sg-abcd" :Status "available"}]
+                  :PerformanceInsightsEnabled false
+                  :IAMDatabaseAuthenticationEnabled false
+                  :StorageType "io1"
+                  :DBInstanceArn "abcdef"}]
+    (is (= {:StorageType "io1"
+            :Iops 100
+            :VpcSecurityGroupIds ["sg-abcd"]
+            :EnablePerformanceInsights false
+            :EnableIAMDatabaseAuthentication false}
+           (lookup/clone-replica-attributes instance)))
+    (is (= {:Iops 100}
+           (lookup/clone-replica-attributes {:Iops 100})))))
