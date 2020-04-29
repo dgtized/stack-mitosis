4062e4a6e28a1a6c7284913935dfa02333788b87
Charles Comstock <dgtized@gmail.com>
2020-04-29 11:14:46 -0500

    Change transition-to to treat unknown state as :in-progress
    This means it should be handled by timeout if it's still failing, however, it
    now logs the unknown status on each invocation, so if it's a status message type
    that is missing it can be accounted for, and if it's just a nil value because
    it's missing it will be visible.

--- src/stack_mitosis/operations.clj
+++ src/stack_mitosis/operations.clj
@@ -1,4 +1,5 @@
-(ns stack-mitosis.operations)
+(ns stack-mitosis.operations
+  (:require [clojure.tools.logging :as log]))
 
 (defn delete
   [id]
@@ -72,7 +73,7 @@
 
   From https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Overview.DBInstance.Status.html"
   [state]
-  (condp contains? (get state :DBInstanceStatus)
+  (condp contains? (:DBInstanceStatus state)
     #{"backing-up" "backtracking" "configuring-enhanced-monitoring"
       "configuring-iam-database-auth" "configuring-log-exports"
       "converting-to-vpc" "creating" "deleting" "maintenance" "modifying"
@@ -87,8 +88,9 @@
     #{"stopped" "available"}
     :done
     ;; unknown or missing
-    nil
-    :failed
+    (do
+      (log/error "Unknown or missing db instance status from" state)
+      :in-progress)
     ))
 
 (defn completed?
--- test/stack_mitosis/operations_test.clj
+++ test/stack_mitosis/operations_test.clj
@@ -1,11 +1,29 @@
 (ns stack-mitosis.operations-test
   (:require [stack-mitosis.operations :as op]
+            [clojure.tools.logging.test :as test-log]
             [clojure.test :refer :all]))
 
 (deftest blocking-operation
   (is (op/blocking-operation? (op/create-replica "bar" "foo")))
   (is (op/blocking-operation? (op/rename "rename-before" "rename-after")))
   (is (not (op/blocking-operation? (op/delete "foo")))))
+
+(deftest transition-to
+  (test-log/with-log
+    (is (= :in-progress (op/transition-to {})))
+    (is (test-log/logged? 'stack-mitosis.operations :error
+                          "Unknown or missing db instance status from {}")))
+  (test-log/with-log
+    (is (= :in-progress (op/transition-to {:DBInstanceStatus nil})))
+    (is (test-log/logged? 'stack-mitosis.operations :error
+                          "Unknown or missing db instance status from {:DBInstanceStatus nil}")))
+  (test-log/with-log
+    (is (= :in-progress (op/transition-to {:DBInstanceStatus ""})))
+    (is (test-log/logged? 'stack-mitosis.operations :error
+                          "Unknown or missing db instance status from {:DBInstanceStatus }")))
+  (is (= :done (op/transition-to {:DBInstanceStatus "available"})))
+  (is (= :failed (op/transition-to {:DBInstanceStatus "failed"})))
+  (is (= :in-progress (op/transition-to {:DBInstanceStatus "modifying"}))))
 
 (deftest completed?
   (let [not-found {:ErrorResponse
