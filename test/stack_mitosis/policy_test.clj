6ab56f5ffdfcc8e82d695747098280f5af19e46f
Charles Comstock <dgtized@gmail.com>
2020-09-19 18:43:22 -0500

    First pass at generating an effect statement from a replace tree operation

--- test/stack_mitosis/policy_test.clj
+++ test/stack_mitosis/policy_test.clj
@@ -1,7 +1,8 @@
 (ns stack-mitosis.policy-test
   (:require [clojure.test :as t :refer [deftest is]]
             [stack-mitosis.operations :as op]
-            [stack-mitosis.policy :as sut]))
+            [stack-mitosis.policy :as sut]
+            [stack-mitosis.planner :as plan]))
 
 (deftest permissions
   (let [instance {:DBInstanceIdentifier "foo" :DBInstanceArn "arn:aws:rds:us-east-1"}]
@@ -14,3 +15,18 @@
     (is (= {:op :DescribeDBInstances}
            (sut/permissions [] (op/describe)))
         "operation only if no database identifier in request")))
+
+(deftest generate
+  (let [instances [{:DBInstanceIdentifier "production" :ReadReplicaDBInstanceIdentifiers ["production-replica"]
+                    :DBInstanceArn "production-arn"}
+                   {:DBInstanceIdentifier "production-replica" :ReadReplicaSourceDBInstanceIdentifier "production"
+                    :DBInstanceArn "production-replica-arn"}
+                   {:DBInstanceIdentifier "staging" :ReadReplicaDBInstanceIdentifiers ["staging-replica"]
+                    :DBInstanceArn "staging-arn"}
+                   {:DBInstanceIdentifier "staging-replica" :ReadReplicaSourceDBInstanceIdentifier "staging"
+                    :DBInstanceArn "staging-replica-arn"}]]
+    (is (= {:effect "Allow"
+            :action [:CreateDBInstanceReadReplica :PromoteReadReplica :ModifyDBInstance :DeleteDBInstance]
+            ;; FIXME: note that create db, promote, and modify may have a different set of resource permissions from delete
+            :resource []}
+           (sut/generate instances (plan/replace-tree instances "production" "staging"))))))
