a6f6b811fff1688f705b8c78b6453513cb9c3043
Charles Comstock <dgtized@gmail.com>
2021-01-14 16:25:13 -0600

    Update comments and leave some hooks for next steps to fix

--- src/stack_mitosis/policy.clj
+++ src/stack_mitosis/policy.clj
@@ -12,16 +12,23 @@
       {:op (:op action)
        :arn (:DBInstanceArn instance)}
       ;; FIXME: this is really gross
+      ;; For create replica cases we don't yet have an instance with an ARN for
+      ;; the newly created id, so we use predict to look ahead a step and use
+      ;; the ARN from the newly created instance.
       (if-let [predicted (lookup/by-id (predict/predict instances action) db-id)]
         {:op (:op action)
          :arn (:DBInstanceArn predicted)}
         ;; fallback to wildcard if we don't recognize
+        ;; This probably should never happen, can we ensure this and drop this
+        ;; case OR should this be a nil arn case?
         {:op (:op action)
          :arn (make-wildcard-arn db-id)}))
     ;; TODO handle ResourceName for ListTagsForResource
     ;; TODO Exclude shell command, and include top level describe?
     {:op (:op action)}))
 
+;; TODO breakup permissions per operation type with better granularity
+;; ie Delete should only have permissions on old-, not temp- or current staging.
 (defn generate [instances operations]
   (let [all-permissions
         (map permissions
