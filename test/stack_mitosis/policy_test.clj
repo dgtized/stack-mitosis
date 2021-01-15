87783e1a4ca580007fd74e002c21aa139ebbefc4
Charles Comstock <dgtized@gmail.com>
2021-01-14 18:26:38 -0600

    Rename make-arn -> fake-arn to clarify intent

--- test/stack_mitosis/policy_test.clj
+++ test/stack_mitosis/policy_test.clj
@@ -4,25 +4,25 @@
             [stack-mitosis.policy :as sut]
             [stack-mitosis.planner :as plan]))
 
-(defn make-arn [name]
+(defn fake-arn [name]
   (str "arn:aws:rds:us-east-1:1234567:db:" name))
 
 (defn example-instances []
   [{:DBInstanceIdentifier "production" :ReadReplicaDBInstanceIdentifiers ["production-replica"]
-    :DBInstanceArn (make-arn "production")}
+    :DBInstanceArn (fake-arn "production")}
    {:DBInstanceIdentifier "production-replica" :ReadReplicaSourceDBInstanceIdentifier "production"
-    :DBInstanceArn (make-arn "production-replica")}
+    :DBInstanceArn (fake-arn "production-replica")}
    {:DBInstanceIdentifier "staging" :ReadReplicaDBInstanceIdentifiers ["staging-replica"]
-    :DBInstanceArn (make-arn "staging")}
+    :DBInstanceArn (fake-arn "staging")}
    {:DBInstanceIdentifier "staging-replica" :ReadReplicaSourceDBInstanceIdentifier "staging"
-    :DBInstanceArn (make-arn "staging-replica")}])
+    :DBInstanceArn (fake-arn "staging-replica")}])
 
 (deftest permissions
-  (let [instance {:DBInstanceIdentifier "foo" :DBInstanceArn (make-arn "foo")}]
-    (is (= {:op :DeleteDBInstance :arn (make-arn "foo")}
+  (let [instance {:DBInstanceIdentifier "foo" :DBInstanceArn (fake-arn "foo")}]
+    (is (= {:op :DeleteDBInstance :arn (fake-arn "foo")}
            (sut/permissions [instance] (op/delete "foo")))
         "only operation if instance is not found")
-    (is (= {:op :DeleteDBInstance :arn (make-arn "foo")}
+    (is (= {:op :DeleteDBInstance :arn (fake-arn "foo")}
            (sut/permissions [instance] (op/delete "foo")))
         "operation and arn if instance is found")
     (is (= {:op :DescribeDBInstances}
@@ -34,12 +34,12 @@
 ;; TODO: handle possible need for RebootInstance if renaming with ApplyImmediatly?
 (deftest generate
   (is (= [(sut/allow [:CreateDBInstanceReadReplica]
-                     (mapv make-arn ["temp-staging" "temp-staging-replica"]))
+                     (mapv fake-arn ["temp-staging" "temp-staging-replica"]))
           (sut/allow [:PromoteReadReplica]
-                     [(make-arn "temp-staging")])
+                     [(fake-arn "temp-staging")])
           (sut/allow [:ModifyDBInstance]
-                     (mapv make-arn ["temp-staging" "temp-staging-replica" "staging-replica" "staging"]))
+                     (mapv fake-arn ["temp-staging" "temp-staging-replica" "staging-replica" "staging"]))
           (sut/allow [:DeleteDBInstance]
-                     (mapv make-arn ["old-staging-replica" "old-staging"]))]
+                     (mapv fake-arn ["old-staging-replica" "old-staging"]))]
          (sut/generate (example-instances)
                        (plan/replace-tree (example-instances) "production" "staging")))))
